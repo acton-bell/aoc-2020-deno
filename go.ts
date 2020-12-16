55ccfeaf61b46b05667abf286a92270e7c082418
Will <william.ingram@concentra.co.uk>
2020-12-16 23:31:41 +0000

    day 1 complete

--- go.ts
+++ go.ts
@@ -3,11 +3,36 @@ import { input } from "./day1.ts";
 console.log("🦕");
 
 // 1a
-const mapped: { [key: number]: number } = {};
-const target = 2020;
-input.forEach((entry) => {
-  mapped[entry] = target - entry;
-  if (mapped[mapped[entry]]) {
-    console.log(entry * (target - entry));
+// For an array of numbers `input`, find the two values which sum to some `target`.
+const findTwoNumbersFromInputThatSumToTarget = (
+  input: number[],
+  target: number,
+) => {
+  const mapped: { [key: number]: number } = {};
+  for (let index = 0; index < input.length; index++) {
+    const element = input[index];
+    mapped[element] = target - element;
+    if (mapped[mapped[element]]) {
+      return [element, mapped[element]];
+    }
   }
-});
+
+  return undefined;
+};
+console.log(findTwoNumbersFromInputThatSumToTarget(input, 2020));
+
+// 1b
+// For an array of numbers `input`, find the three values which sum to some `target`.
+const findThreeNumbersFromInputThatSumToTarget = () => {
+  for (let index = 0; index < input.length; index++) {
+    const element = input[index];
+    const twoNumbers = findTwoNumbersFromInputThatSumToTarget(
+      input,
+      2020 - element,
+    );
+    if (twoNumbers) {
+      return [element, ...twoNumbers];
+    }
+  }
+};
+console.log(findThreeNumbersFromInputThatSumToTarget());
